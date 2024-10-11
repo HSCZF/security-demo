72b103a10349c0a7a1b8b9e931fb4a6d900445c4
HSCZF <1337081497@qq.com>
2024-10-11 14:50:51 +0800

    @EnableWebSecurity

--- src/main/java/com/example/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/example/securitydemo/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.securitydemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -16,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
  * @date 2024/10/7 - 19:35
  */
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity    (在springboot，这个注解可以不要)
 public class SecurityConfig {
 
 
